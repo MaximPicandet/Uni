ee1b89b340926ed3bdf4ad40e4f475557cc8b63b
Maxim Picandet <maximpicandet@gmail.com>
2026-05-01 18:36:25 +0200

    Aufgabenteil b)
    Programm erweitert, um Aufgabenteil b) zu berechnen. Ich habe für den trockenen Anteil den linken Summanden der Formel für den Brechungsindex genutzt, für den feuchten Anteil den rechten.
    
    Somit habe ich den optischen Weg für rein trockene und rein feuchte Atmosphären bestimmt, die Summe beider Anteile ergibt jedoch nicht den optischen Weg einer "realen" Atmosphäre.

--- OSF/uebung_03.py
+++ OSF/uebung_03.py
@@ -20,6 +20,14 @@ Aufgabe a:
 """
 
 import numpy as np
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    zenithDelay: float
+    zenithDelayTrocken: float
+    zenithDelayFeucht: float
+    truePath: float
 
 def optischerWeg(file):
     """
@@ -32,34 +40,48 @@ def optischerWeg(file):
 
     Returns
     -------
-    zenithDelay : float64
-    opticPath : float64
+    Result object with
+        zenithDelay : float
+            total zenith delay with both dry and wet components
+        zenithDelayTrocken : float
+            zenith delay using only the left summand of the equation for the refraction index
+            assumption vapor pressure e = 0
+        zenithDelayFeucht : float
+            zenith delay using only the right summand of the equation for the refraction index
+        truePath : float
+            total length of the true path (no delay)
 
     """
     
     data = np.loadtxt(file, delimiter=",", skiprows=1)
+
+    nTrocken = 1 + 77.6e-8 * data[:,1] / data[:,2]
+    nFeucht = 1 + (64.8e-8 + 3.776e-3 / data[:,2]) * data[:,3] / data[:,2]
     
-    n = 1 + (
-        77.6e-8 * data[:,1] / data[:,2] +
-        (64.8e-8 + (3.776e-3) / data[:,2]) * data[:,3] / data[:,2]
-        )
+    n = 1 + (nTrocken - 1) + (nFeucht - 1)
     
     ds = np.empty(data[:,0].size);
-    print(data[:,0].size)
     
     ds[1:-1] = (data[2:,0] - data[:-2,0]) / 2
     ds[0] = data[1,0] - data[0,0]
     ds[-1] = data[-1,0] - data[-2,0]
     
     L = n * ds
+    Ltrocken = nTrocken * ds
+    Lfeucht = nFeucht * ds
+    
     L = L.sum()
+    Ltrocken = Ltrocken.sum()
+    Lfeucht = Lfeucht.sum()
     
     zenithDelay = L - data[-1,0]
-    return zenithDelay, L
+    zenithDelayTrocken = Ltrocken - data[-1,0]
+    zenithDelayFeucht = Lfeucht - data[-1,0]
+    return Result(zenithDelay, zenithDelayTrocken, zenithDelayFeucht, data[-1,0])
     
 
 fileTropisch = "../Übungen/uebung_03_Daten_tropisch.dat"
 fileSubarktisch = "../Übungen/uebung_03_Daten_subarktischer-winter.dat"
 
-ZDtropisch, OWtropisch = optischerWeg(fileTropisch)
-ZDsubarktisch, OWsubarktisch = optischerWeg(fileSubarktisch)
+resultTropisch = optischerWeg(fileTropisch)
+resultSubarktisch = optischerWeg(fileSubarktisch)
